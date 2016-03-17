4957ff98cad7313a138c37dd94c9590fe12ddf8c
Vagun Ilya <vaguniv@iek.ru>
2016-03-17 09:59:58 +0300

    move /config -> /etc

--- lib/core/factory.php
+++ lib/core/factory.php
@@ -13,19 +13,19 @@ abstract class PFactory {
    * Init common classes
    *
    */
-   
+
   private static $dir, $config;
   public static function init()
   {
     self::$dir = dirname(__FILE__).'/../';
     require_once(self::getDir().'/core/mysql.php');
   }
-  
+
   /**
 	 * Get library directory.
 	 *
 	 */
-  
+
   public static function getDir()
   {
     return self::$dir;
@@ -48,11 +48,11 @@ abstract class PFactory {
 
 		return self::$database[$dataBase];
 	}
-  
+
   private static $dbConfig;
   private static function getDbconfig() {
     if(isset(self::$dbConfig)) return self::$dbConfig;
-    $config = file_get_contents(self::getDir().'/../config/db');
+    $config = file_get_contents(self::getDir().'/../etc/db');
     preg_match_all('#(.*)=(.*)$#m', $config, $_mathes);
     self::$dbConfig = new stdClass();
     for($i = 0; $i < 4; $i++) {
@@ -62,27 +62,27 @@ abstract class PFactory {
       }
     return self::$dbConfig;
   }
-  
+
   /**
    * Set global option
    *
-   */  
-  
+   */
+
   private static $options = array();
   public static function setOpt($name, $value)
   {
     self::$options[$name] = $value;
   }
-  
+
   /**
    * Get global option
    *
    */
-  
+
   public static function getOpt($name)
   {
     return isset(self::$options[$name]) ? self::$options[$name] : null;
-  } 
-  
+  }
+
 }
 
